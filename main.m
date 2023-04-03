c6d1954003057a5afd9711aba615be5bc7cfbf25
Paulo Monteiro de Carvalho Monson <paulo.monson@unesp.br>
2023-04-02 21:07:59 -0300

    Added MATLAB files.

--- main.m
+++ main.m
@@ -1,11 +1,13 @@
-D1 = [1.5 1];
-D2 = [3.9268 3.1429];
+D1 = [70 40];
+D2 = [330 228];
 
 disp("Euclidian distance: " + EucDist(D1, D2))
 disp("City-block (D4) distance: " + Dist4(D1, D2))
 disp("Chessboard (D8) distance: " + Dist8(D1, D2))
 
-pdist2(D1, D2, "euclidean") == EucDist(D1, D2);
+disp("Euclidian distance: " + pdist2(D1, D2, "euclidean"))
+disp("City-block (D4) distance: " + pdist2(D1, D2, "cityblock"))
+disp("Chessboard (D8) distance: " + pdist2(D1, D2, "chebychev"))
 
 %%
 
